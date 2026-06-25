bcd9d47789cec710837cac92228e7d7829256ccc
AntoinePassemiers <antoine.passemiers@gmail.com>
2026-06-25 20:49:08 +0200

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
-  //site: 'https://AntoinePassemiers.github.io',
-  //base: '/Personal-Website',
+  site: 'https://AntoinePassemiers.github.io',
+  base: '/Personal-Website',
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
